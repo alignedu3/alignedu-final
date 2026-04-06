c37c0c3fe71f274180323fd701e23f34ed803457
alignedu3 <ryan@alignedu.net>
2026-04-06 14:56:36 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
+// postcss.config.js
 module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    'postcss-preset-env': {
+      stage: 0, // This enables modern CSS features
+    },
+    autoprefixer: {}, // Ensures compatibility with older browsers
   },
 };
