a354c7f9ad370b68499883d9054db06b3abc8a17
alignedu3 <ryan@alignedu.net>
2026-04-06 15:13:02 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
-// postcss.config.js
 module.exports = {
-  plugins: [
-    'postcss-preset-env', // For modern CSS syntax compatibility
-    'autoprefixer' // To ensure compatibility with older browsers
-  ]
+  plugins: {
+    'postcss-preset-env': {
+      stage: 1,
+    },
+    autoprefixer: {},
+  },
 };
