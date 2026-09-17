df8ece3f6999aa73c09f5ed43f2b40fd59eaf3b4
alignedu3 <ryan@alignedu.net>
2026-09-17 13:39:40 -0500

    Pass checkout session to signup from server

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,15 @@
-import { Suspense } from 'react';
 import SignUpPage from '@/components/SignUpPage';
 
-export default function SignUp() {
-  return (
-    <Suspense fallback={<main style={{ minHeight: '100vh' }} />}>
-      <SignUpPage />
-    </Suspense>
-  );
+type SignUpRouteProps = {
+  searchParams: Promise<{
+    checkout_session_id?: string | string[];
+  }>;
+};
+
+export default async function SignUp({ searchParams }: SignUpRouteProps) {
+  const params = await searchParams;
+  const rawSessionId = params.checkout_session_id;
+  const checkoutSessionId = Array.isArray(rawSessionId) ? rawSessionId[0] : rawSessionId || null;
+
+  return <SignUpPage checkoutSessionId={checkoutSessionId} />;
 }
